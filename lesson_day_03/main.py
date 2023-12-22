5daafe9450f8d75f4a1c788bc3ff690c40846c13
tornado-06 <tornado-06@admin-6s-iMac.local>
2023-12-22 17:54:21 +0800

    add: lesson day 03

--- lesson_day_03/main.py
+++ lesson_day_03/main.py
@@ -7,15 +7,20 @@ print(operand)
 operation = input('Give me operation')
 print(operation)
 
-if operation == "+":
-    result = operand + operator
-elif operation == "-":
-    result = operator - operand
-elif operation == "*":
-    result = operator * operand
-    print(result)
-elif operation == "%":
-    result = operator % operand
-    print(result)
 
     
+try:
+    if operation == "+":
+        result = operand + operator
+    elif operation == "-":
+        result = operator - operand
+    elif operation == "*":
+        result = operator * operand
+        print(result)
+    elif operation == "%":
+        result = operator % operand
+        print(result)
+    else:
+        print('operation is not supperted')
+except:
+    print('give me number')
