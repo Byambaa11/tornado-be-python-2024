145b21603aa68d9181e6a5a8e2433b130450259b
tornado-06 <tornado-06@admin-6s-iMac.local>
2024-03-31 17:40:47 +0800

    add: lesson_day_38

--- lesson_day_19/articles/views.py
+++ lesson_day_19/articles/views.py
@@ -5,6 +5,11 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Article
+from django.views.generic.edit import (
+                UpdateView,
+                DeleteView,
+                CreateView,
+)
 
 class ArticleListView(ListView):
     model = Article
@@ -23,3 +28,12 @@ class ArticleDeleteView(DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
+    
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = "article_new.html"
+    fields = (
+        "title",
+        "body",
+        "author",)
+    
--- lesson_day_19/core/user/models.py
+++ lesson_day_19/core/user/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+import uuid
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
